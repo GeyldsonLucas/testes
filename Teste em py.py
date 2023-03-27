e28b632301f323ca061fd0ca23b1cdcf0088cb21
Lucas Mendes <mendeslucas0602@ufpi.edu.br>
2023-03-27 16:41:22 -0300

    Condicional para par ou impar

--- Teste em py.py
+++ Teste em py.py
@@ -2,6 +2,13 @@
 print("Hello World")
 
 
-a = input("Answer this question:")
+a = input("Choose a number:")
 
-print(a)
+
+a = int(a)
+print(type(a))
+
+if(a %2 == 0):
+        print("um numero par")
+else:
+    print("E um numero impar")
